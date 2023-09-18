22efb43892bb562c5f7d7497fdf6dad398712c19
Matthew <myzerith1@gmail.com>
2023-09-18 17:59:25 -0400

    added functions and a few defined variables
    I will use this for calculations of the calculator

---
+++ javascript.js
@@ -0,0 +1,27 @@
+let a;
+let b;
+let operator;
+let sum;
+
+function operate(){
+    a =
+    b =
+    operator =
+    //Nedd to create a way to call this function
+    function()
+}
+function add(){
+sum = a + b
+}
+
+function subtract(){
+sum = a - b
+}
+
+function multiply(){
+sum = a * b
+}
+
+function divide(){
+sum = a / b
+}
